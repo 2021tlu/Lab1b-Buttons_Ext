d60e47f353b25f154efd61b40072016977478308
Thomas L <2021tlu@tjhsst.edu>
2020-10-14 10:27:32 -0400

    Add reset functionality

--- app/src/main/java/com/luthomas/lab1b_buttonsextension/MainActivity.java
+++ app/src/main/java/com/luthomas/lab1b_buttonsextension/MainActivity.java
@@ -12,10 +12,11 @@ public class MainActivity extends AppCompatActivity {
 
     // Views
     // TextView title = findViewById(R.id.title);
-    TextView numText = findViewById(R.id.numText);
-    TextView turnText = findViewById(R.id.turnText);
+    TextView numText;
+    TextView turnText;
     int count = 0;
     int turn = 0;
+    int overFlag = 0;
     /* Button one = findViewById(R.id.one);
     Button two = findViewById(R.id.two);
     Button three = findViewById(R.id.three); */
@@ -24,23 +25,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        numText = findViewById(R.id.numText);
+        turnText = findViewById(R.id.turnText);
     }
-
-    public void changeNumText(View view){
-        Log.i("buttonResponse:", (String) view.getTag());
-        count += Integer.parseInt((String) view.getTag());
-        turn += 1;
-        numText.setText("" + count + funText(count));
+    public void resetGame(View view){
+        count = 0;
+        turn = 0;
+        overFlag = 0;
+        numText.setText("" + count);
         turnText.setText(getTurnText(turn));
     }
+    public void changeNumText(View view) {
+        Log.i("buttonResponse:", (String) view.getTag());
+        count += Integer.parseInt((String) view.getTag());
+        if (count >= 21) {
+            if (overFlag == 0) {
+                overFlag = 1;
+                numText.setText("" + count);
+                String overText = getOverText(turn); // find over text with current turn
+                turnText.setText(overText);
+            }
+        }
+        else {
+            turn += 1;
+            numText.setText("" + count); //+ funText(count));
+            turnText.setText(getTurnText(turn));
+        }
+    }
     public String funText(int count){
-        String fun = ":O";
+        String fun = " :D";
         for(int i = 1; i < count/5; i++)
-            fun += "O";
+            fun += "D";
         return fun;
     }
-    public String getTurnText(int count){
-        return "Player " + ((count%2)+1) + "'s Turn";
+    public String getTurnText(int turn){
+        return "Player " + ((turn%2)+1) + "'s Turn";
+    }
+    public String getOverText(int turn){ // turn given is loser's turn
+        return "Player " + (((turn+1)%2)+1) + " Wins!";
     }
 }
